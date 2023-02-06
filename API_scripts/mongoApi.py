5b0c51b412110e8df286006a2252fb5a2c567ffb
Jia Jin Chan <57128827+ChanJiaJin@users.noreply.github.com>
2023-02-06 06:10:00 +0000

    added API for Gantt chart items fetch, add and update functions

--- API_scripts/mongoApi.py
+++ API_scripts/mongoApi.py
@@ -1,89 +1,116 @@
-import pymongo  
+import pymongo
 
 #initiate MongoDB and asign it to a variable
-mongo = pymongo.MongoClient("mongodb+srv://chanjiajin:S8438106%23c@cluster0.b2zj7en.mongodb.net/?retryWrites=true&w=majority")
+mongo = pymongo.MongoClient(
+  "mongodb+srv://chanjiajin:S8438106%23c@cluster0.b2zj7en.mongodb.net/?retryWrites=true&w=majority"
+)
+
 
 #creating new project entry
 #inserting (Title), (Abbreviation) and (Code) into {projects} db
 #creating a new db using (Title)
 def newProject(title, abbv, code):
 
-    if title:
-        #creating new db
-        newDb = mongo[title]
+  if title:
+    #creating new db
+    newDb = mongo[title]
 
-        #inserting collections into new db
-        details = newDb.details
-        information = newDb.information
-        issues = newDb.issues
-        flow = newDb.flow
-        gantt = newDb.gantt
-        docs = newDb.docs
+    #inserting collections into new db
+    details = newDb.details
+    information = newDb.information
+    issues = newDb.issues
+    flow = newDb.flow
+    gantt = newDb.gantt
+    docs = newDb.docs
 
-        #inserting new project details for 
-        #projects list table
-        insertDoc = {
-            "title": title,
-            "abbv": abbv,
-            "code": code
-        }
-        
-        details.insert_one(insertDoc)
+    #inserting new project details for
+    #projects list table
+    insertDoc = {"title": title, "abbv": abbv, "code": code}
+
+    details.insert_one(insertDoc)
 
 
-#for grabbing all projects and their details 
+#for grabbing all projects and their details
 #for projects list table in proj_page
 def fetchDetails():
-    dbNames = mongo.list_database_names()
-    projectsList = []
+  dbNames = mongo.list_database_names()
+  projectsList = []
 
-    #looping over all db
-    for db in dbNames:
-    
-        #to remove db without details collection
-        if db != "users" and db != "admin" and db != "local":
+  #looping over all db
+  for db in dbNames:
 
-            #access collection
-            details = mongo[db]["details"]
+    #to remove db without details collection
+    if db != "users" and db != "admin" and db != "local":
 
-            #to find all info and exclude _id
-            info = details.find({},{"_id": 0})
+      #access collection
+      details = mongo[db]["details"]
 
-            #to loop over the returned cursor from find()
-            #and put them in a dictionary
-            for i in info:
-                title = i["title"]
-                abbv = i["abbv"]
-                code = i["code"]
-                projDetails = [title, abbv, code]
-                #append dictionary to array
-                projectsList.append(projDetails)
-        else:
-            pass
+      #to find all info and exclude _id
+      info = details.find({}, {"_id": 0})
 
-    return projectsList
+      #to loop over the returned cursor from find()
+      #and put them in a dictionary
+      for i in info:
+        title = i["title"]
+        abbv = i["abbv"]
+        code = i["code"]
+        projDetails = [title, abbv, code]
+        #append dictionary to array
+        projectsList.append(projDetails)
+    else:
+      pass
+
+  return projectsList
+
 
 #for finding project db and passing all data into it
 def fetchProject(abbv):
-    dbNames = mongo.list_database_names()
+  dbNames = mongo.list_database_names()
 
-    for db in dbNames:
-        if dbNames != abbv:
-            pass
-        else:
-            return mongo[db]
+  for db in dbNames:
+    if dbNames != abbv:
+      pass
+    else:
+      return mongo[db]
 
 
 #for adding in gantt chart items
 def addTask(number, title, task, start, end):
-    db = mongo[title]
+  db = mongo[title]
 
-    insertDoc = {
-        "taskNo": number,
-        "task": task,
-        "start": start,
-        "end": end
-    }
+  insertDoc = {"taskNo": number, "task": task, "start": start, "end": end}
 
-    gantt = db.gantt
-    gantt.insert_one(insertDoc)
+  gantt = db["gantt"]
+  gantt.insert_one(insertDoc)
+
+
+#to fetch all tasks to display in gantt chart
+#function will be used in both proj_details and proj_settings
+def fetchTasks(title):
+  db = mongo[title]
+  gantt = db["gantt"]
+  tasks = []
+
+  for i in gantt:
+    tasks.append(i)
+
+  return tasks
+
+
+#To update database of any changes done to the gantt chart
+#All data to be updated regardless of how many changes there are
+#This update will work everytime the settings stack widget is changed
+def updateGantt(title, update):
+  db = mongo[title]
+  gantt = db["gantt"]
+
+  #to extract old values
+  old = []
+  for i in gantt:
+    old.append(i)
+
+  #to replace old values with new ones
+  for old in old:
+    for new in update:
+      value = {"$set": new}
+      gantt.update_one(old, value)
